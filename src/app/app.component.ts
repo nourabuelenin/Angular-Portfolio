4e643b4ab9f14760b405035b6f2f99d65028778e
nourabuelenin <nourabuelenin@gmail.com>
2025-02-06 20:16:51 +0200

    portfolio using Angular - first try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroSectionComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
